a196b2d4ae1cd21df82ea8eae9c7fc85b315b766
Malika <dusmagambetovaaa@gmail.com>
2025-03-25 01:48:34 +0500

    feat: 5 passing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -64,8 +64,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const nextDate = new Date(date);
+  nextDate.setDate(date.getDate() + ((5 - date.getDay() + 7) % 7 || 7));
+  return nextDate;
 }
 
 /**
@@ -79,8 +81,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
